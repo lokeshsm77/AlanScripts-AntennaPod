b2b31452da6bef35f9c6d75daa9130a39f39faca
Alan Android <55826308+lokeshsm77@users.noreply.github.com>
2020-04-27 18:46:01 +0530

    Update AlanVoiceScripts.js
    Made some changes to voice script, added screen check while navigating back to the previous screen

--- AlanVoiceScripts.js
+++ AlanVoiceScripts.js
@@ -2,25 +2,33 @@ visualHints("The easy-to-use, flexible and open-source podcast manager for Andro
 
 
 let searchPodcast = visual(state => state.screen === "Search Podcast");
+let podcastDetails = visual(state => state.screen === "Podcast Details");
+
 intent("Hello Alan", 
        reply("welcome to Antenna podcast manager with Alan voice integration")
       );
 
 searchPodcast = context(() =>{
     title("Search podcast");
-    intent("Search podcast", 
+    intent(searchPodcast, "Search podcast", 
        reply("What do you want to search",
              follow("Search for (a|the|) $(PODCAST* (.*))", 
-                        p=>{
-                            let song = p.PODCAST.value;
-                            if(song === 0){
+                       async p=>{
+                            let podcast = p.PODCAST.value;
+                            if(podcast === 0){
                                 p.play("Can't search on emtpy field")
-                            } else if(song < 2){
+                            } else if(podcast < 2){
                                 p.play("Search text must be 3 characters");
                             } else {
-                                p.play({command:"add-text", value:song});
+                                p.play({command:"add-text", value:podcast});
                                 p.play("Do you want to proceed with the search, Yes or No?")
-                                p.then(proceedSearch, {SONG:song});
+                                let action = await p.then(confirmSearch);
+                                if(action === 'yes'){
+                                     p.play({command:"search", value:podcast});
+                                } else if(action === 'no'){
+                                    p.play({command:"clear-text", value:""});
+                                    p.play("Canceling your search");
+                                }   
                             }
                         }
                    )
@@ -30,27 +38,13 @@ searchPodcast = context(() =>{
 
 
 
-let proceedSearch = context(() =>{
+let confirmSearch = context(() =>{
     title("Proceed Search Action");
-    follow("$(ACTION yes|no)", 
+    follow("$(ACT yes|no)", 
                  p=>{  
-                    let action = p.ACTION.value;
-                    if(action != undefined){
-                        switch(action){
-                            case 'yes':
-                                p.play({command:"search", value:p.SONG});
-                                break;
-                            case 'no':
-                                p.play({command:"clear-text", value:""});
-                                p.play("Canceling your search");
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-        
-                }
-          )
+                     return p.resolve(p.ACT.value);
+                 }
+          );
     }
 );
 
@@ -58,22 +52,26 @@ let proceedSearch = context(() =>{
 let podcastResults = context(() =>{
     title("Podcast Results");
     follow("(Select|Tell me the details of|) $(ORDINAL) (th|st|nd|)", 
-       p=>{
+        p=>{
             if(p.ORDINAL.number > p.userData.userData.count){
                 let itemList = (p.userData.userData.count === 1? "item": "items") + " in the list";
                 p.play("There are " + p.userData.userData.count + itemList);
             } else{
                 p.play({command:"select-podcast", value:p.ORDINAL.number});
-                p.then(podcastFeedOPs);
+                 p.then(podcastFeedOPs);
             }
         }
      );
     
      follow("(go|navigate) back", 
        p=>{
-            p.play("Navigating back to previous screen");
-            p.play({command:"go-back", value:"back"});
-            p.then(searchPodcast);
+             if((p.visual.screen !== "Search Podcast")){
+                p.play("Navigating back to previous screen");
+                p.play({command:"go-back", value:"back"});
+                p.then(searchPodcast);
+             } else {
+                p.play("You are in home screen", "Already in Dash board");
+             }
         }
       );
     
@@ -82,26 +80,27 @@ let podcastResults = context(() =>{
 
 let podcastFeedOPs = context(()=>{
     title("Podcast Feed Operations");
-    intent("yes", p=>{
+  
+    intent(podcastDetails, "yes", p=>{
         p.play({command: "read-description", value: "Read description"});
         p.play("reading podcast description");    
     })
-    intent("close", p=>{
+    intent(podcastDetails, "close", p=>{
         p.play("Navigating back");
         p.play({command: "go-back", value: "back"});
         p.then(podcastResults);
     })
-    intent("home", "go home", p=>{
+    intent(podcastDetails, "home", "go home", p=>{
         p.play("Navigating back to home screen");
         p.play({command: "go-home", value: "home"});
         p.then(searchPodcast);
     })
     
-    intent("no", p=>{
+    intent(podcastDetails, "no", p=>{
         p.play("Ok");
     })
     
-    intent("subscribe", p=>{
+    intent(podcastDetails, "subscribe", p=>{
         p.play({command: "subscribe-podcast", value: "subscribe"});
         p.play("Subscribing the podcast");    
     })
@@ -109,7 +108,7 @@ let podcastFeedOPs = context(()=>{
 
 
 let backToSearchPodcast = context(() =>{
-    follow("read", 
+    follow("go (back|home|)", 
        p=>{
             p.play({command:"go-back", value:"back"});
             p.play("Navigating back to previous screen");
